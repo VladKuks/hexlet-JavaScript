b57428d92bfbd3e68caa92b35886a5575e5ba0f4
VladKuks <vladkuksinskiy@gmail.com>
2020-11-27 10:26:52 +0300

    Prettier changes

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-import _ from 'lodash';
+import _ from "lodash";
 
-console.log('Hello, Hexlet!');
-console.log(_.last(['one', 'two']));
+console.log("Hello, Hexlet!");
+console.log(_.last(["one", "two"]));
 
 const fn = (a, b) => {
-  const c = a + b; return c;
+  const c = a + b;
+  return c;
 };
